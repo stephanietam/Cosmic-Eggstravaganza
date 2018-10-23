36f5af8533ae37a3bf076752db2313c669a2536c
Michel Li <li.michel8@gmail.com>
2018-10-23 10:47:27 -0700

    added side menu for pets/inventory/shop/options

---
+++ Cosmic Eggstravaganza/Assets/Scripts/ResourceDisplay.cs
@@ -0,0 +1,36 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ResourceDisplay : MonoBehaviour
+{
+	public GameState gameState;
+	public Text foodText;
+	public Text starsText;
+
+	// Use this for initialization
+	void Start ()
+	{
+		SetFood();
+		SetStars();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		SetFood();
+		SetStars();
+	}
+
+	void SetFood ()
+	{
+		foodText.text = gameState.GetFood().ToString() + " Food";
+	}
+
+	void SetStars ()
+	{
+		starsText.text = gameState.GetStars().ToString() + " Stars";
+	}
+}
+
