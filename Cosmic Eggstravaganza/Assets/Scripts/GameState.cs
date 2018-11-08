b6300330d18a3d52c97e758fe5d5ddb68334804a
stephanietam <stepht750@gmail.com>
2018-11-08 10:34:39 -0800

    gamestate moves with scene

--- Cosmic Eggstravaganza/Assets/Scripts/GameState.cs
+++ Cosmic Eggstravaganza/Assets/Scripts/GameState.cs
@@ -2,20 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 namespace Assets.Scripts {
     public class GameState : MonoBehaviour
     {
-        private readonly DateTime dateTime;
+        public readonly DateTime dateTime;
 
-		private int food;
-        private int stars;
+        public int food;
 
-        private readonly List<Creature> creatures;
+        public int stars;
 
-        private int creatureCount;
+        public readonly List<Creature> creatures;
 
-        private List<GameObject> creatureObjects;
+        public int creatureCount;
+
+        public List<GameObject> creatureObjects;
 
         public GameState()
         {
@@ -29,11 +31,7 @@ namespace Assets.Scripts {
         public void Start()
         {
             // Create the first creature
-            GameObject newCreature = new GameObject("Creature" + this.creatureCount++);
-            newCreature.AddComponent<Creature>();
-            newCreature.GetComponent<Creature>().Hatch();
-            creatureObjects.Add(newCreature);
-            Debug.Log("Creature created!");
+            this.CreateCreature();
         }
 
         public DateTime GetDateTime()
@@ -64,6 +62,69 @@ namespace Assets.Scripts {
                 creatures.Add(creatureObjects[i].GetComponent<Creature>());
             }
             return creatures;
+        }
+
+        public List<GameObject> GetCreatureObjects()
+        {
+            return this.creatureObjects;
+        }
+
+        public static GameState Instance;
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                DontDestroyOnLoad(transform.gameObject);
+                Instance = this;
+            }
+            else if (Instance != this)
+            {
+                Destroy(transform.gameObject);
+            }
+        }
+
+        private void CreateCreature()
+        {
+            // Create gameobject
+            GameObject newCreature = new GameObject("Creature " + this.creatureCount++);
+            newCreature.AddComponent<SpriteRenderer>();
+            newCreature.AddComponent<CapsuleCollider2D>();
+
+            // Choose random pet
+            List<string> imageFiles = new List<string>{"1","2","3","4","5","6a", "6b", "6c", "7" };
+            System.Random random = new System.Random();
+            int randomImageInt = random.Next(0,9);
+            Debug.Log("Randomimageint: " + randomImageInt);
+
+            // Set image
+            Texture2D tex = Resources.Load<Texture2D>(imageFiles[randomImageInt]) as Texture2D;
+            Debug.Log("image name: " + tex.name);
+            Sprite sprite = new Sprite();
+            sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f));
+            SpriteRenderer SR = newCreature.GetComponent<SpriteRenderer>();
+            SR.sprite = sprite;
+
+            // Set size
+            Transform transform = newCreature.GetComponent<Transform>();
+            Vector3 mi = transform.localScale;
+            mi.y = 0.1f;
+            mi.x = 0.1f;
+            transform.localScale = mi;
+
+            // Set collider size
+            CapsuleCollider2D collider = newCreature.GetComponent<CapsuleCollider2D>();
+            Vector2 v = collider.size;
+            v.x = 10f;
+            v.y = 10f;
+            collider.size = v;
+
+            // Add creature component
+            newCreature.AddComponent<Creature>();
+            newCreature.GetComponent<Creature>().Hatch();
+
+            // Add to list of creatures
+            creatureObjects.Add(newCreature);
+            Debug.Log("Creature created!");
         }
     }
 }
---
+++ Cosmic Eggstravaganza/Assets/Scripts/PetLibrary.cs
@@ -0,0 +1,57 @@
+﻿using Assets.Scripts;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PetLibrary : MonoBehaviour {
+    public GameState gameState;
+    public GameObject noPet;
+    public GameObject displayObject;
+    public Creature displayCreature;
+    public Text statsText;
+    private List<GameObject> creatures;
+
+    // Use this for initialization
+    void Start() {
+        GameObject gameObject = GameObject.FindGameObjectWithTag("GameState");
+        this.gameState = gameObject.GetComponent<GameState>();
+        this.creatures = gameState.GetCreatureObjects();
+        if (this.creatures.Count > 0)
+        {
+            // Hide all the creatures
+            for (int i = this.creatures.Count-1; i >= 0; --i)
+            {
+                Debug.Log(i + " set inactive");
+                this.creatures[i].SetActive(false);
+            }
+
+            // set display creature
+            this.displayObject = this.creatures[0];
+            this.displayCreature = this.displayObject.GetComponent<Creature>();
+
+            // Show the stats for the displayed creature
+            SetStats(this.statsText, this.displayCreature);
+        }
+        else
+        {
+            // Show no pet message
+            this.noPet.SetActive(true);
+        } 
+    }
+
+    public void Update()
+    {
+        if (this.creatures.Count > 0)
+        {
+            SetStats(this.statsText, this.displayCreature);
+        }
+    }
+
+    public void SetStats(Text text, Creature creature)
+    {
+        text.text = creature.name + "\n" +
+                "Happiness: " + creature.GetHappiness().GetPoints().ToString() + "\n" +
+                "Hunger: " + creature.GetHunger().GetPoints().ToString() + "\n" +
+                "Hygene: " + creature.GetHygene().GetPoints().ToString() + "\n";
+    }
+}
