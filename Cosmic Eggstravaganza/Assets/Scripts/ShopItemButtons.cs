0054d89260120150b16fb5662a4cab6c63b4baff
stephanietam <stepht750@gmail.com>
2018-11-16 13:48:32 -0800

    Updated store

--- Cosmic Eggstravaganza/Assets/Scripts/ShopItemButtons.cs
+++ Cosmic Eggstravaganza/Assets/Scripts/ShopItemButtons.cs
@@ -5,31 +5,25 @@ using UnityEngine.UI;
 
 public class ShopItemButtons : MonoBehaviour
 {
-    // modes (determines what menu shows up):
-    // 0 = none
-    // 1 = egg
-    // 2 = food
-    // 3 - medicine
-    private int currentMode;
-
-    public GameObject panel;
-
-    public Text panelTitle;
-
-    public Text panelDescription;
-
     public Text starsQuantity;
 
-    public Image itemImage;
-
     public GameState gameState;
+
+    public GameObject popupPanel;
+
+    public Text popupText;
+
+    public Button popupButton;
+
+    public Text ownEggs;
+
+    public Text ownMedicine;
+
+    public Text ownFood;
 
     // Use this for initialization
     void Start()
     {
-        panel.SetActive(false);
-        this.currentMode = 0;
-
         GameObject gameObject = GameObject.FindGameObjectWithTag("GameState");
         this.gameState = gameObject.GetComponent<GameState>();
 
@@ -39,139 +33,76 @@ public class ShopItemButtons : MonoBehaviour
         {
             creatures[i].SetActive(false);
         }
+
+        this.ownEggs.text = "Own: " + this.gameState.creatureCount;
+        this.ownFood.text = "Own: " + this.gameState.GetFood();
+        this.ownMedicine.text = "Own: " + this.gameState.GetMedicine();
     }
 
     private void Update()
     {
-        if (this.currentMode == 1)
-        {
-            this.starsQuantity.text = "Pets owned: " + this.gameState.creatureCount + "\n" +
-                "Stars: " + this.gameState.GetStars();
-        }
-        else if (this.currentMode==2)
-        {
-            this.starsQuantity.text = "Owned: " + this.gameState.GetFood() + "\n" +
-                "Stars: " + this.gameState.GetStars();
-        }
-        else if (this.currentMode==3)
-        {
-            this.starsQuantity.text = "Owned: " + this.gameState.GetMedicine() + "\n" +
-                "Stars: " + this.gameState.GetStars();
-        }
+        this.starsQuantity.text = "Stars: " + this.gameState.GetStars().ToString();
     }
 
-    private void SetObjects()
+    public void BuyEgg()
     {
-        if (this.currentMode == 0)
+        if (this.gameState.GetStars() - 50 >= 0)
         {
-            panel.SetActive(false);
+            // buying egg
+            this.gameState.AddStars(-50);
+
+            // Create gameobject
+            this.gameState.CreateCreature();
+            this.ownEggs.text = "Pet count: " + this.gameState.creatureCount;
+            Popup("You bought an egg!");
         }
         else
         {
-            panel.SetActive(true);
-            if (this.currentMode == 1)
-            {
-                Debug.Log("Eggs button clicked");
-                panelTitle.text = "Egg";
-                panelDescription.text = "Buy a brand new egg!";
-
-                Texture2D tex = Resources.Load<Texture2D>("egg") as Texture2D;
-                Sprite sprite = new Sprite();
-                sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f));
-                itemImage.sprite = sprite;
-            }
-            else if (this.currentMode == 2)
-            {
-                Debug.Log("Food button clicked");
-                panelTitle.text = "Food";
-                panelDescription.text = "Food to feed your pets.";
-
-                Texture2D tex = Resources.Load<Texture2D>("egg") as Texture2D;
-                Sprite sprite = new Sprite();
-                sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f));
-                itemImage.sprite = sprite;
-            }
-            else if (this.currentMode == 3)
-            {
-                Debug.Log("Medicine button clicked");
-                panelTitle.text = "Medicine";
-                panelDescription.text = "Medicine to heal your pets.";
-
-                Texture2D tex = Resources.Load<Texture2D>("egg") as Texture2D;
-                Sprite sprite = new Sprite();
-                sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f));
-                itemImage.sprite = sprite;
-            }
+            // pop up warning
+            Popup("You don't have enough stars!");
         }
     }
 
-    public void ClosePanel()
+    public void BuyFood()
     {
-        this.currentMode = 0;
-        SetObjects();
-    }
-
-    public void SetEggPanel()
-    {
-        this.currentMode = 1;
-        SetObjects();
-    }
-
-    public void SetFoodPanel()
-    {
-        this.currentMode = 2;
-        SetObjects();
-    }
-
-    public void SetMedicinePanel()
-    {
-        this.currentMode = 3;
-        SetObjects();
-    }
-
-    public void BuyButton()
-    {
-        if (this.currentMode==1)
+        if (this.gameState.GetStars() - 10 >= 0)
         {
-            if (this.gameState.GetStars()-50>=0)
-            {
-                // buying egg
-                this.gameState.AddStars(-50);
+            // buying food
+            this.gameState.AddStars(-10);
+            this.gameState.AddFood(1);
+            this.ownFood.text = "Own: " + this.gameState.GetFood().ToString();
+            Popup("You bought food!");
+        }
+        else
+        {
+            Popup("You don't have enough stars!");
+        }
+    }
 
-                // Create gameobject
-                this.gameState.CreateCreature();
-            }
-            else
-            {
-                // pop up warning
-                Debug.Log("Not enough money!");
-            }
-        }
-        else if (this.currentMode==2)
+    public void BuyMedicine()
+    {
+        if (this.gameState.GetStars() - 10 >= 0)
         {
-            if (this.gameState.GetStars() - 10 >= 0)
-            {
-                // buying food
-                this.gameState.AddStars(-10);
-                this.gameState.AddFood(1);
-            }
-            else
-            {
-                Debug.Log("Not enough money!");
-            }
+            // buying medicine
+            this.gameState.AddStars(-10);
+            this.gameState.AddMedicine(1);
+            this.ownMedicine.text = "Own: " + this.gameState.GetMedicine().ToString();
+            Popup("You bought medicine!");
         }
-        else if (this.currentMode==3)
+        else
         {
-            if (this.gameState.GetStars() - 10 >= 0)
-            {
-                // buying medicine
-                this.gameState.AddStars(-10);
-                this.gameState.AddMedicine(1);
-            }
-            else 
-            {
-                Debug.Log("Not enough money!");
-            }
+            Popup("You don't have enough stars!");
         }
+    }
+
+    private void Popup(string message)
+    {
+        this.popupPanel.SetActive(true);
+        this.popupText.text = message;
+    }
+
+    public void ClosePopup()
+    {
+        this.popupPanel.SetActive(false);
     }
 }
