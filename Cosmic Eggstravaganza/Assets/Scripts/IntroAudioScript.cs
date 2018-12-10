9ad77ad4806616e688bfe0c388a5de68d9646571
Michel Li <li.michel8@gmail.com>
2018-12-09 21:46:49 -0800

    added intro sound fx

---
+++ Cosmic Eggstravaganza/Assets/Scripts/IntroAudioScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroAudioScript : MonoBehaviour
+{
+	public AudioSource backgroundMusic;
+	public AudioSource explosionSound;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+
+	public void Explosion()
+	{
+		backgroundMusic.Stop();
+		explosionSound.Play();
+	}
+}
+
